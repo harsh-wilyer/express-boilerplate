b9a1b15ce1bcf50ef5b26e5708fe1dba26cd4dd8
harsh-wilyer <harsh@wilyer.com>
2024-12-23 14:15:32 +0530

    initial commit'

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const PORT = 8000;
+
+// Define a route for the root URL
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
